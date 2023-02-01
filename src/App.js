ace913e77cff6e8a03c2662d105fd37b2dee8fce
7piyushdev <dchlamydia@gmail.com>
2023-02-01 13:43:32 +0530

    first commit task7

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+
+
+  return (
+    <div className="App">
+        <h1>Let's Learn React</h1>
+        <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
+
+ 
