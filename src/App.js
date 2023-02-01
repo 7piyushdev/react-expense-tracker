154dab38a9abd6e7ca6635cc9ff4814687a59ec4
7piyushdev <dchlamydia@gmail.com>
2023-02-01 16:54:37 +0530

    task9

--- src/App.js
+++ src/App.js
@@ -2,16 +2,50 @@ import React from "react";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-
-
+  const expenses = [
+    {
+      id: 1,
+      title: "Paper",
+      amount: 266.99,
+      date: new Date(2021, 3, 10),
+      LocationOfExpenditure: "chandigarh",
+    },
+    {
+      id: 2,
+      title: "Wooden Chair",
+      amount: 216.99,
+      date: new Date(2021, 3, 10),
+      LocationOfExpenditure: "moon",
+    },
+    {
+      id: 3,
+      title: "Pencils",
+      amount: 166.99,
+      date: new Date(2021, 3, 10),
+      LocationOfExpenditure: "klka",
+    },
+    {
+      id: 4,
+      title: "Ball Pens",
+      amount: 223.99,
+      date: new Date(2021, 3, 10),
+      LocationOfExpenditure: "hydrabad",
+    },
+  ];
   return (
     <div className="App">
-        <h1>Let's Learn React</h1>
-        <ExpenseItem />
+      <h1>Let's Learn React</h1>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          LocationOfExpenditure={expense.LocationOfExpenditure}
+        />
+      ))}
     </div>
   );
 }
 
 export default App;
-
- 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 import React from "react";
+import './ExpenseItem.css'
 
-function ExpenseItem() {
-  
+function ExpenseItem(props) {
+   
 
   return (
-    <div>
-      <div>Expense Items</div>
-      <div>Food Rs 10 </div>
-      <div>Petrol Rs 100</div>
-      <div> Movies Rs 200</div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div>{props.LocationOfExpenditure}</div>
+        <div className="expense-item__price">{props.amount}</div>
+       
+      </div>
     </div>
   );
 }
