25bec8da5ca5b8df13cc54457fc591a62d7c4303
7piyushdev <dchlamydia@gmail.com>
2023-02-02 11:25:55 +0530

    task12

--- src/App.js
+++ src/App.js
@@ -1,49 +1,33 @@
-import React from "react";
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react';
 
-function App() {
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
-      id: 1,
-      title: "Paper",
-      amount: 266.99,
-      date: new Date(2021, 1, 15),
-      LocationOfExpenditure: "chandigarh",
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id: 2,
-      title: "Wooden Chair",
-      amount: 216.99,
-      date: new Date(2021, 8, 1),
-      LocationOfExpenditure: "moon",
-    },
-    {
-      id: 3,
-      title: "Pencils",
-      amount: 166.99,
-      date: new Date(2021, 3, 22),
-      LocationOfExpenditure: "klka",
-    },
-    {
-      id: 4,
-      title: "Ball Pens",
-      amount: 223.99,
-      date: new Date(2021, 6, 20),
-      LocationOfExpenditure: "hydrabad",
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
   ];
   return (
-    <div className="App">
-      <h1>Let's Learn React</h1>
-      {expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          LocationOfExpenditure={expense.LocationOfExpenditure}
-        />
-      ))}
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
